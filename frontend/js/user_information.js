757a4f808db09a2233ca688eb1bb916b7af7c392
adamhoof <adam.hofman1@gmail.com>
2023-11-19 21:59:15 +0100

    Add user_information.css file for specific page requirements Improve structure of user_information.html Add js logic for disabling and enabling elements

--- frontend/js/user_information.js
+++ frontend/js/user_information.js
@@ -1,7 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("edit").addEventListener("click", function(){
-        console.log("clicked");
+function disableElements(elements) {
+    elements.forEach(function (element) {
+        element.setAttribute('disabled', 'true');
     });
 
+}
 
+function enableElements(elements) {
+    elements.forEach(function (element) {
+        element.removeAttribute('disabled');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const edit_checkbox = document.getElementById("edit_checkbox");
+
+    if (!edit_checkbox.checked) {
+        const allElements = document.querySelectorAll('.disableable');
+        disableElements(allElements)
+    }
+
+    edit_checkbox.addEventListener("change", function () {
+
+        const allElements = document.querySelectorAll('.disableable');
+
+        if (edit_checkbox.checked) {
+            enableElements(allElements)
+        } else {
+            disableElements(allElements);
+        }
+    });
 });
