581be13422428688f8bee8c39ea78e05b4e8c23d
adamhoof <adam.hofman1@gmail.com>
2023-12-11 17:19:39 +0100

    "Prevent direct access to user.php and refactor User class
    Direct access security feature is added in user.php, it now serves a 403 unauthorized access page when accessed directly, enhancing script safety. User class has been refactored to exclude the $pictureUrl property and its corresponding usage, since it is determined to be redundant in the user model context."

--- backend/user.php
+++ backend/user.php
@@ -1,22 +1,36 @@
 <?php
 
+if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
+    echo '<!DOCTYPE html>';
+    echo '<html lang="en">';
+    echo '<head>';
+    echo '<meta charset="UTF-8">';
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+    echo '<title>Get outa here</title>';
+    echo '</head>';
+    echo '<body>';
+    echo '<h1>403 - Access forbidden</h1>';
+    echo '</body>';
+    echo '</html>';
+    die();
+};
+
 class User
 {
     public int $id;
+
     public string $guid;
     public string $password;
-    public string $pictureUrl;
 
-    public function __construct($id, $guid, $password, $pictureUrl)
+    public function __construct($id, $guid, $password)
     {
         $this->id = $id;
         $this->guid = $guid;
         $this->password = $password;
-        $this->pictureUrl = $pictureUrl;
     }
 
     public static function emptyUser(): User
     {
-        return new self(-1, "", "", "");
+        return new self(-1, "", "");
     }
 }
