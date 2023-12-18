4da1ce87e397bb6d0f8c986625cdbd2ffcdf9904
adamhoof <adam.hofman1@gmail.com>
2023-12-18 09:40:38 +0100

    Change names

---
+++ backend/dbCredentials.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Database connection credentials.
+ *
+ * This file defines constants for database connection.
+ * These constants are used to connect to the MySQL database.
+ */
+
+/**
+ * Database hostname.
+ */
+const HOSTNAME = "localhost";
+
+/**
+ * Database username.
+ */
+const USERNAME = "myuser";
+
+/**
+ * Database password.
+ */
+const PASSWORD = "mypassword";
+
+/**
+ * Database name.
+ */
+const DATABASE = "mydatabase";
+
+/**
+ * Database port number.
+ */
+const PORT = "3306";
--- backend/db_credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-const HOSTNAME = "localhost";
-const USERNAME = "myuser";
-const PASSWORD = "mypassword";
-const DATABASE = "mydatabase";
-const PORT = "3306";
